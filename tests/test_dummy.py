7f45ffde569835a4dd1ce5e07a209240a19cd1aa
Paul Teng <plankp@outlook.com>
2022-10-10 11:35:44 -0400

    Add automated test coverage (#6)
    * Add automated test coverage
    
    * Bump version
    
    * Add basic dir structure
    
    For documenting that --cov should have the source dir and not the test dir

---
+++ tests/test_dummy.py
@@ -0,0 +1,8 @@
+# XXX: Just a file to trick pytest into thinking that there are tests
+#      Will need to go once there are real tests
+
+import project
+
+def test_i_just_always_pass():
+  assert True
+
