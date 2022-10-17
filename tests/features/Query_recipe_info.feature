d63f75dfd436abf70b131c898e2c9d69e3b8c980
Paul Teng <plankp@outlook.com>
2022-10-16 20:39:39 -0400

    Add the gherkin tests

---
+++ tests/features/Query_recipe_info.feature
@@ -0,0 +1,22 @@
+Feature: Query recipe info
+
+As a user I would like to query recipe info so I can learn more about it
+
+  Background:
+    Given a user
+    And a recipe named "Recipe Foo"
+    And with a tag named "test-only"
+    And with a tag named "foobars"
+    And a recipe named "Recipe Bar"
+    And with a tag named "baz"
+    And with an ingredient "salt" of "5 tbsp"
+
+  Scenario: Search for tags
+    When I query tags of "Recipe Foo"
+    Then I should have tags
+      ["test-only", "foobars"]
+
+  Scenario: Search for ingredients
+    When I query ingredients of "Recipe Bar"
+    Then I should have ingredients
+      [ ["salt", "5 tbsp"] ]
---
+++ tests/test_Query_recipe_info.py
@@ -0,0 +1,80 @@
+import json
+from pytest_bdd import (
+    given,
+    scenario,
+    then,
+    when,
+    parsers
+)
+from project.ingredient_query import *
+from project.recipe_query import *
+from project.tag_query import *
+
+@scenario('features/Query_recipe_info.feature', 'Search for tags')
+def test_search_for_tags(app, postgresql):
+    pass
+
+@scenario('features/Query_recipe_info.feature', 'Search for ingredients')
+def test_search_for_ingredients(app, postgresql):
+    pass
+
+@given('a user')
+def a_user(postgresql):
+    cur = postgresql.cursor()
+    cur.execute("INSERT INTO accounts VALUES (DEFAULT, 'Dummy', 'Dummy', 'Dummy')")
+    postgresql.commit()
+
+@given(parsers.parse('a recipe named "{name}"'), target_fixture="recipe_id")
+def a_recipe_named(name, postgresql):
+    cur = postgresql.cursor()
+    cur.execute("""
+        INSERT INTO recipes VALUES (DEFAULT, %s, NULL, NULL, 'go', 1)
+        RETURNING id""", (name,))
+    recipe_id = cur.fetchone()[0]
+    postgresql.commit()
+    return recipe_id
+
+@given(parsers.parse('with a tag named "{name}"'))
+def with_a_tag_named(name, recipe_id, postgresql):
+    cur = postgresql.cursor()
+    cur.execute("INSERT INTO tags VALUES (DEFAULT, %s) RETURNING id", (name,))
+    tag_id = cur.fetchone()[0]
+    cur.execute("INSERT INTO recipe_tags VALUES (%s, %s)", (recipe_id, tag_id))
+    postgresql.commit()
+
+@given(parsers.parse('with an ingredient "{name}" of "{quant}"'))
+def with_an_ingredient__of(name, quant, recipe_id, postgresql):
+    cur = postgresql.cursor()
+    cur.execute("""
+        INSERT INTO ingredients VALUES (DEFAULT, %s, %s, %s)
+        """, (name, quant, recipe_id))
+    postgresql.commit()
+
+@when(parsers.parse('I query ingredients of "{recipe}"'), target_fixture="res")
+def i_query_tags_of(recipe):
+    (recipes, _) = search_recipes_by_filter(recipe, [], 0)
+    return get_ingredients_of_recipe(recipes[0][0])
+
+
+@when(parsers.parse('I query tags of "{recipe}"'), target_fixture="res")
+def i_query_tags_of(recipe):
+    (recipes, _) = search_recipes_by_filter(recipe, [], 0)
+    return get_tags_of_recipe(recipes[0][0])
+
+@then(parsers.parse('I should have tags\n{tags}'))
+def i_should_have_tags(tags, res):
+    tags = json.loads(tags)
+    assert len(res) == len(tags)
+
+    for (_, tag) in res:
+        assert tag in tags
+
+@then(parsers.parse('I should have ingredients\n{ingredients}'))
+def i_should_have_ingredients(ingredients, res):
+    ingredients = json.loads(ingredients)
+    assert len(res) == len(ingredients)
+
+    for (_, name, quant, _) in res:
+        assert [name, quant] in ingredients
+
+
