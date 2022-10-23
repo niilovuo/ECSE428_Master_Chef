cd81c11cb07de633e68771bbdd1cafd9be8833af
Lilirose200x <48535851+Lilirose200x@users.noreply.github.com>
2022-10-23 01:27:54 -0400

    Update test_comment.py

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -66,3 +66,4 @@ def test_delete_comment_by_id_Exception(monkeypatch, app):
     monkeypatch.setattr("project.db.CommentRepo.select_by_id", lambda id: 1)
     err = delete_comment_by_id(1, 1, 1)
     assert err == "Could not delete comment, please try again"
+
