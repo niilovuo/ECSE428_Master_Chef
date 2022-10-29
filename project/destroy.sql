21d5f57a105f94b710ecf6d757daf5e8bf53f0ed
Paul Teng <plankp@outlook.com>
2022-10-29 17:42:11 -0400

    Avoid triggering pytest-postgresql on windows (#146)
    * Let's see what happens
    
    * Update readme
    
    * Apparently I also can't spell

---
+++ project/destroy.sql
@@ -0,0 +1,8 @@
+-- A SQL script if you absolutely want to destroy all data
+
+DROP TABLE IF EXISTS accounts CASCADE;
+DROP TABLE IF EXISTS recipes CASCADE;
+DROP TABLE IF EXISTS ingredients CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS tags CASCADE;
+DROP TABLE IF EXISTS recipe_tags CASCADE;
