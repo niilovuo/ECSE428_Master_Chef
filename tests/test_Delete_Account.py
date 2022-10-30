9d57eef391efd9110b8a882c435df42f0d6d0f02
anotherLostKitten <eggismagicchickensquared@gmail.com>
2022-10-30 18:09:03 -0400

    can we test flash messages then

--- tests/test_Delete_Account.py
+++ tests/test_Delete_Account.py
@@ -57,7 +57,9 @@ def the_account_deleted_successfully(user_id):
     assert account_id is None
 
 @then('the system will display an error message')
-def the_account_not_deleted(a_user, res):
+def the_account_not_deleted(a_user, res, client):
     account_id = search_account_by_id(a_user)
     assert account_id is not None
-    assert res.status_code != 200
+    with client.session_transaction() as session:
+        flash_messages = session['_flashes']
+        assert len(flash_messages) != 0
