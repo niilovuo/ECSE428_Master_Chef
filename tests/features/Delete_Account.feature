e0e12c0288c3a6dd106bb4ed559a355639921449
Sia Ham <59781764+SiaH319@users.noreply.github.com>
2022-10-30 17:35:25 -0400

    updated but fixture still needed

--- tests/features/Delete_Account.feature
+++ tests/features/Delete_Account.feature
@@ -5,25 +5,15 @@ I would like to delete my account for the Master Chef System
 So that my account information no longer exists on the system database.
 
   Background:
-    Given the following accounts exist in the system:
-    [
-      [ "id", "name", "email", "password" ],
-      [ "1", "User1", "user1@gmail.com", "password1" ],
-
-    ]
-
+    Given the user with id "1" exist in the system
 
   Scenario: A logged in user attempts to their delete account (Normal Flow)
-
-    Given the user "User1" exists in the system
-    Given the user is logged into the system
+    Given the user with id "1" is logged into the system
     When attempting to delete their account
     Then the user account with id "1" does not exist
 
   Scenario: Logged out user attempts to delete account (Error Flow)
-
-    Given the user "User1" exists in the system
-    Given the user is not logged into the system
-    When attempting to delete account
+    Given the user with id "1" is not logged into the system
+    When attempting to delete their account
     Then the system will display an error message
 
--- tests/test_Delete_Account.py
+++ tests/test_Delete_Account.py
@@ -28,38 +28,39 @@ def a_user(postgresql):
     postgresql.commit()
     return user_id
 
-@given(parsers.parse('the user "{title}"  exists in the system'), target_fixture="user_id")
-def the_user_exists_in_the_system(postgresql, a_user):
+@given('the user with id "{user_id}" exist in the system', target_fixture="user_id")
+def user_info_exisits_int_the_system(user_id):
     cur = postgresql.cursor()
-    cur.execute("""
-        INSERT INTO accounts VALUES (DEFAULT, %s, NULL, NULL, 'go', %s)
-        RETURNING id""", (title, a_user,))
+    cur.execute("INSERT INTO accounts VALUES (1, 'User1', 'user1@gmail.com', 'password1') RETURNING id")
     user_id = cur.fetchone()[0]
     postgresql.commit()
     return user_id
 
+@given('the user with id "{user_id}" is logged into the system', target_fixture="user_id")
+def user_is_logged_into_the_system(client, a_user):
+    with client.session_transaction() as session:
+        session['id'] = a_user
+        return a_user
 
-@given('the user is logged into the system', target_fixture='user_login')
-def user_is_logged_into_the_system(user_id):
-    return user_id
+@given('the user with id "{user_id}" is not logged into the system', target_fixture="user_id")
+def the_user_is_not_logged_into_the_system(client):
+    with client.session_transaction() as session:
+        session['id'] = None
+        return None
 
-
-@given('the user is not logged into the system', target_fixture='user_login')
-def user_is_logged_into_the_system(user_id):
-    return None
-
-@when(parsers.parse('attempting to delete their account'), target_fixture='res')
-def attempting_to_delete_comment( user_login):
-    res = delete_account_by_id(user_login)
+@when('attempting to delete their account')
+def attempting_to_delete_comment(user_id):
+    res = delete_account_by_id(user_id)
     return res
 
 @then(parsers.parse('the user account with id "{user_id}" does not exist'))
 def the_account_deleted_successfully(user_id):
-    accound_id = search_account_by_id(user_id)
-    assert accound_id is None
+    account_id = search_account_by_id(user_id)
+    assert account_id is None
 
 @then('the system will display an error message')
 def the_account_not_deleted(user_id):
-    accound_id = search_account_by_id(user_id)
-    assert accound_id is not None
+    res = delete_account_by_id(user_id)
+    account_id = search_account_by_id(user_id)
+    assert account_id is not None
     assert res is "Unknown error occurred. Please try again later"
