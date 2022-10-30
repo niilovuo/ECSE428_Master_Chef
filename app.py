57ee7751cc1535a618ff7370e30d35a6180da6c2
Tyler <70815823+T-Syme@users.noreply.github.com>
2022-10-30 15:26:14 -0400

    Comment UI (#143)
    * Make UI function with endpoints
    
    * Refactor comments with delete button
    
    * Update delete comment button
    
    Co-authored-by: Paul Teng <plankp@outlook.com>

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def create_app(setup_db=True):
     @app.route("/user", methods=["GET"])
     def get_current_user():
         if 'id' in session:
-            return "Someone is in", 200
+            return str(session.get("id")), 200
         return "No user", 401
 
     @app.route("/search")
@@ -168,7 +168,7 @@ def create_app(setup_db=True):
         allow_edits = session.get('id') == recipe["author"]
         return render_template("/recipe.html",
                                recipe=recipe, author=author, tags=tags, ingredients=ingredients,
-                               allow_edits=allow_edits)
+                               allow_edits=allow_edits, user=session.get('id'))
                                
     @app.route("/recipes/create")
     def render_create_recipe():
